543f087f65a2604458a37e26676172003f40e0c8
PizzaNCoding88 <umbertonardiello@gmail.com>
2025-06-30 15:46:25 +0100

    score page v0.2

--- app/scores/page.js
+++ app/scores/page.js
@@ -1,14 +1,34 @@
 'use client'
 import React from 'react'
 import { useQuiz } from "../context/QuizContext";
+import Logo from "../../public/assets/images/logo.png";
+import Image from "next/image";
+import Trophy from "../../public/assets/images/trophy.png";
+import './page.css';
 
 const Results = () => {
- const [,,,, correctAnswers, wrongAnswers] = useQuiz();
+ const [questions,,,, correctAnswers, wrongAnswers] = useQuiz();
   return (
-    <div>
-      <div className='text-white'>correct answers: {correctAnswers}</div>
-      <div className='text-white'>wrong answers: {wrongAnswers}</div>
-    </div>
+    <div className="container">
+        <div className="image-container">
+          <Image alt="logo" src={Logo} className="logo" />
+        </div>
+        <div className="central-part-score">
+          <div className='trophy-container'>
+            <Image alt="trophy" src={Trophy} className="trophy" />
+          </div>
+          <div className='completed'>
+            <h1>Quiz Completed</h1>
+          <p>Here's how you did</p>
+          </div>
+          <div className='score-container'>
+            <div className='score'>
+              <p>{correctAnswers}/{questions.length}</p>
+            </div>
+          </div>
+        </div>
+        /
+      </div>
   )
 }
 
